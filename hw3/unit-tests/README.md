82be2ff7fcdd60d5bcdbae190eac23f9eaecbbc6
Ruyan <fading.jade@gmail.com>
2014-09-17 14:17:32 -0700

    added hw3 unit test

--- hw3/unit-tests/README.md
+++ hw3/unit-tests/README.md
@@ -3,3 +3,6 @@ Homework 3 Unit Tests
 
 This directory contains the unit tests for homework 3 that you can use to test 
 your program before you submit. 
+
+To use these unit tests, replace "Map.h" in the header of main.cpp with the 
+correct file (if necessary), compile, and run.
---
+++ hw3/unit-tests/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <exception>
+#include <stdexcept>
+#include "Map.h"
+//#include "Set.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	//testing Maps
+	//map object
+	Map<string, string> newMap;
+
+	cout << "Size of an empty map should be: " << newMap.size() << endl; //answer = 0
+
+	try{
+		newMap.remove("Aaron"); //should do nothing
+	}catch(exception &e){
+		cout << e.what() << endl;
+	}
+
+	//now adding things to the map
+	string people[] = {"Aaron", "Afsin", "Alana", "David", "Johnathan", "Nikhil", "Joseph", "Matthew", "Douglass", "Ruyan", "Ian", "Connor", "Stephen", "Ellen", "Erika"};
+	for(int i = 0; i < 15; i++){
+		newMap.add(people[i], people[i] + "@usc.edu");
+	}
+	cout << "Now the size of the map should be 15! Actual size: " << newMap.size() << endl;
+
+	bool success = false;
+	cout << "Alana's email is: " << newMap.get("Alana", success) << endl; //should be Alana@usc.edu
+	if(success == false){
+		cout << "There is something wrong with your get function!" << endl;
+	}
+
+	//add a different value for alana, see if the add function is working properly
+	try{
+		newMap.add("Alana", "ashine@usc.edu");
+	}catch(exception &e){
+		cout << e.what() << endl;
+	}
+	success = false;
+	cout << "Alana's email should still be Alana@usc.edu. My output: " << newMap.get("Alana", success) << endl;
+	//if Alana's email wasn't Alana@usc.edu, then something is wrong with the add function
+
+	//try to get someone's email that isn't there
+	try{
+		success = false;
+		cout << newMap.get("Henry", success) << endl;
+		if(success == true){
+			cout << "Something is wrong with the get function" << endl;
+		}
+	}catch(exception &e){
+		cout << e.what() << endl;
+	}
+
+	//remove every third person from the map
+	for(int i = 0; i < 15; i+=3){
+		newMap.remove(people[i]);
+	}
+	cout << "Now size of the map should be 10! Actual size: " << newMap.size() << endl;
+
+	//now testing merge
+	Map<string, string> mapOther;
+	for(int i = 0; i < 15; i++){
+		mapOther.add(people[i], people[i] + "@usc.edu");
+	}
+	//now mapOther has 15 items and newMap has 10. The five items previously deleted from newMap should be added back in
+	newMap.merge(mapOther);
+
+	cout << "Now size of map should be 15. Actual size: " << newMap.size() << endl;
+
+	//print out newMap using get to make sure all emails have been added back in
+	int newMapSize = newMap.size();
+	bool test;
+	for(int i = 0; i < newMapSize; i++){
+		test = false;
+		cout << newMap.get(people[i], test) < endl;
+		if(success == false){
+			cout << "There's something wrong with the get function, recheck your code!" << endl;
+			break;
+		}
+	}
+
+	return 0;
+}
