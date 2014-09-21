ea321bc5e495f27f66b889b6b644eb0414f40be6
Ruyan Chen <colortheskyjade@users.noreply.github.com>
2014-09-21 02:58:18 -0700

    added comments to sample solution

--- hw2/solutions/hw2q6_solution.cpp
+++ hw2/solutions/hw2q6_solution.cpp
@@ -4,20 +4,25 @@
  */
 double findMaxLearning(double work[], double learning[], int classCount, double workSoFar, double learningSoFar, double maxWork, double &maxLearning)
 {
+	// Look through all the classes from 0 to the given max.
 	for(int i = 0; i < classCount; i++)			
 	{
 
 		workSoFar = workSoFar + work[i];	
 		learningSoFar = learningSoFar + learning[i];
 
+		// If adding that class doesn't exceed maximum work, then try adding it.
 		if (workSoFar <= maxWork)			
 		{
+			// If adding this class increases the maximum learning, set it as the new maximum learning.
 			if(learningSoFar > maxLearning)
 				maxLearning = learningSoFar;
 		
+			// Now recurse on all classes up to this class.
 			findMaxLearning (work, learning, i, workSoFar, learningSoFar, maxWork, maxLearning); 
 		}
 
+		// Now undo the calculations so we can try again on a different class.
 		workSoFar = workSoFar - work[i];	
 		learningSoFar = learningSoFar - learning[i];
 	}
